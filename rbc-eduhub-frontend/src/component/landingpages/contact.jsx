4c7c3980bc8c8e9e33da9c0dd4b87b9debb17cae
IshimwePatience <ishimweaugstin12@gmail.com>
2025-11-13 13:59:59 +0200

    11/13/2025

--- rbc-eduhub-frontend/src/component/landingpages/contact.jsx
+++ rbc-eduhub-frontend/src/component/landingpages/contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
     <div className="min-h-screen bg-gray-50">
    {/* Two Column Layout */}
       <div className="max-w-7xl mx-auto px-4 py-16">
-        <div className="flex flex-col lg:flex-row gap-8">
+        <div className="flex flex-col lg:flex-row gap-3">
           
           {/* Main Content - Left Side */}
           <div className="lg:w-2/3">
@@ -29,7 +29,7 @@ function Contact() {
               <p className="text-base text-gray-700 leading-relaxed mb-6">
                 Rwanda Biomedical Centre is located in Kigali, Rwanda. We welcome visits by appointment for institutional partnerships and consultations.
               </p>
-              <div className="rounded-2xl overflow-hidden shadow-2xl border-4 border-gray-200">
+              <div className="overflow-hidden">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127641.82987352748!2d30.01985795!3d-1.9440726999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19dca4258ed8e797%3A0xe9f3b4e887122e67!2sKigali%2C%20Rwanda!5e0!3m2!1sen!2sus!4v1234567890123!5m2!1sen!2sus"
                   width="100%"
